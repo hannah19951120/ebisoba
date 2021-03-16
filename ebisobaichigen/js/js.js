a41a617b73c0b699acf2d881d0bb318d7dd1f640
哈哈哈哈娜 <76716052+hannah19951120@users.noreply.github.com>
2021-03-16 16:04:26 +0800

    0316

--- ebisobaichigen/js/js.js
+++ ebisobaichigen/js/js.js
@@ -1,44 +1,38 @@
 $(document).ready(function() {
-    
+    $('.hero__title').addClass('hero__title-clear')
+
     // 按鈕滑動
    $(".scrollBtn").bind("click",function(){
     event.preventDefault();
-    var id=$(this).attr("data-scrollName");
+    let id=$(this).attr("data-scrollName");
 
-    var target_top = $("#scroll"+id).offset().top; // 取得目標區塊的y座標
-    var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+    let target_top = $("#scroll"+id).offset().top; // 取得目標區塊的y座標
+    let $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
           $body.animate({
                 scrollTop: target_top
           }, 800);
     })
     
-    // QA展開
-    $('.question__list>li>a').click(function(){
-        event.preventDefault();
-        $(this).siblings().slideToggle();
-        $(this).toggleClass('question__list--active');
-        $(this).parent().siblings().find('.question__list_answer').slideUp();
-        $(this).parent().siblings().find('a').removeClass('question__list--active');
+    // 更多活動
+   $("#scrollBottom").bind("click",function(){
+    event.preventDefault();
+    let id=$(this).attr("data-scrollName");
+
+    let target_top = $("#scroll"+id).offset().top; // 取得目標區塊的y座標
+    let $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+          $body.animate({
+                scrollTop: target_top-200
+          }, 800);
     })
 
-    // Burger 手機選單
-    $('.header__burger').click(function(){
-        event.preventDefault();
-        $(this).toggleClass('header__burger--active');
-        $('.header__nav').toggleClass('header__nav--active');
+    $(window).scroll(function(){
+        let target_top = $("#scrollMore").offset().top-500;
+        if($(window).scrollTop()>=target_top){
+            $('#scrollBottom').fadeOut(100);
+        }else{
+            $('#scrollBottom').fadeIn(100);
+        }
     })
-
-
-    // 新手指南Swiper
-    $('.tutorialHeader__list').click(function(){
-        event.preventDefault();
-        $(this).addClass('tutorialHeader__list--active');
-        $(this).siblings().removeClass('tutorialHeader__list--active');
-
-        let target = $(this).attr('data-tutorial');
-        $('.tutorialSwiper').addClass('d-none');
-        $('#'+target).removeClass('d-none');
-    })
-
+    
 
 });
